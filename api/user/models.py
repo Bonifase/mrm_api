be55799553d196633984a5aba851a140a81fb663
dnuwa <daniel.nuwa@gmail.com>
2019-01-17 22:34:23 +0300

    feat(remove-user-role-entity): user_role entity removed - Removed user_role entity - Added an association table for the users_roles
    [#163188330]

--- api/user/models.py
+++ api/user/models.py
@@ -1,13 +1,19 @@
-from sqlalchemy import (Column, String, Integer)
+from sqlalchemy import (Column, String, Integer, ForeignKey, Table)
 from sqlalchemy.orm import relationship
 from sqlalchemy.schema import Sequence
 
 from helpers.database import Base
 from utilities.utility import Utility, validate_empty_fields
-from api.user_role.models import UsersRole  # noqa: F401
 from api.notification.models import Notification  # noqa: F401
 from api.feedback.models import Feedback  # noqa: F401
 from api.response.models import Response  # noqa: F401
+
+users_roles = Table(
+    'users_roles',
+    Base.metadata,
+    Column('user_id', Integer, ForeignKey('users.id')),
+    Column('role_id', Integer, ForeignKey('roles.id'))
+)
 
 
 class User(Base, Utility):
@@ -17,10 +23,11 @@ class User(Base, Utility):
     location = Column(String, nullable=True)
     name = Column(String, nullable=False)
     picture = Column(String, nullable=True)
-    roles = relationship('Role', secondary='users_roles')
     notification_settings = relationship(
         'Notification', cascade="all, delete-orphan")
     feedback = relationship('Feedback', cascade="all, delete-orphan")
+    associated_roles = relationship(
+        'Role', secondary="users_roles", backref=('users_association'), lazy="joined")
 
     def __init__(self, **kwargs):
         validate_empty_fields(**kwargs)
